99c737cdca2825f1ad9db7c8eb2853ff4c91b39d
Stone.Hou <xiangxing985529@163.com>
2015-06-25 23:53:25 +0800

    Fibonacci series
    Fibonacci series

--- python/calc.py
+++ python/calc.py
@@ -4,3 +4,16 @@ print('hello, world')
 print(300 + 100 +200)
 # 然后，通过命令给hello.py以执行权限：
 # chmod a+x hello.py
+
+# Python 3: Fibonacci series up to n
+def fib(n):
+  a, b = 0, 1
+  while a < n:
+  print(a, end=' ')
+  a, b = b, a+b
+  print()
+fib(1000)
+#0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987
+
+# https://www.pythonanywhere.com/
+https://www.pythonanywhere.com/
